c206f25b68f86354bd2c1c1cd75bdeaece41bd7d
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-09-25 14:25:49 +0900

    [update] Add Firebase dependencies
    Firebaseの依存関係を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'firebase_options.dart';
 import 'navigation/route_page.dart';
 import 'viewmodel/friendtask_name_viewmodel.dart';
 import 'viewmodel/mytask_name_viewmodel.dart';
@@ -13,7 +15,11 @@ final mytaskNameProvider =
     StateNotifierProvider<MyTaskNameViewModel, List<String>>(
         (_) => MyTaskNameViewModel());
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ProviderScope(
       child: RoutePage(),
