cf712368deb113bbce669cced9062f518e803898
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-11-15 23:33:12 +0900

    [update] ログイン画面のアップデート

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -16,25 +16,44 @@ class LoginScreen extends HookConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                style: Theme.of(context).textTheme.headline5,
-                children: [
-                  const TextSpan(
-                    text: 'ごほうび',
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                gradient: LinearGradient(
+                  colors: [Colors.blue[400]!, Colors.pink[300]!],
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(90),
+                  color: Colors.white,
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: Theme.of(context).textTheme.headline5,
+                    children: [
+                      const TextSpan(
+                        text: 'ごほうび',
+                      ),
+                      TextSpan(
+                        text: ' ToDo ',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .headline3
+                                ?.fontSize),
+                      ),
+                      const TextSpan(
+                        text: 'アプリ',
+                      ),
+                    ],
                   ),
-                  TextSpan(
-                    text: ' ToDo ',
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize:
-                            Theme.of(context).textTheme.headline3?.fontSize),
-                  ),
-                  const TextSpan(
-                    text: 'アプリ',
-                  ),
-                ],
+                ),
               ),
             ),
             Column(
@@ -43,29 +62,53 @@ class LoginScreen extends HookConsumerWidget {
                 SignInButton(
                   Buttons.Google,
                   text: "Googleでログイン",
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
                   onPressed: () => googleProvider.googleLogin(() {
                     AutoRouter.of(context).replace(const HomeRoute());
                     AutoRouter.of(context).removeUntil((route) => false);
                   }),
                 ),
+                const SizedBox(height: 20),
                 SignInButton(
                   Buttons.Twitter,
                   text: "Twitterでログイン",
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
                   onPressed: () {},
                 ),
+                const SizedBox(height: 20),
                 SignInButton(
                   Buttons.AppleDark,
                   text: "Appleでログイン",
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
                   onPressed: () {},
                 ),
+                const SizedBox(height: 20),
                 SignInButton(
                   Buttons.FacebookNew,
                   text: "Facebookでログイン",
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
                   onPressed: () {},
                 ),
+                const SizedBox(height: 20),
                 SignInButton(
                   Buttons.Email,
                   text: "メールでログイン",
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
                   onPressed: () {},
                 )
               ],
