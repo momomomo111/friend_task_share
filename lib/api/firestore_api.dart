174918d9784b89aa6de3c1028e63059afe089c8b
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-12-04 14:58:55 +0900

    [fix] formatの修正

--- lib/api/firestore_api.dart
+++ lib/api/firestore_api.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../domain/user_data.dart';
 
 import '../domain/task.dart';
+import '../domain/user_data.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
